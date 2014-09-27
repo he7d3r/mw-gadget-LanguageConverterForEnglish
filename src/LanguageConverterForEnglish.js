562ac2b25313cd547eccafcb08bc5bc3fe32a6bf
Helder <he7d3r@gmail.com>
2014-09-27 20:49:01 -0300

    Apply MediaWiki Coding Conventions
    Validated with jscs 1.6.2, using preset "wikimedia"

--- src/LanguageConverterForEnglish.js
+++ src/LanguageConverterForEnglish.js
@@ -4,37 +4,37 @@ if ( window.LanguageConverter === undefined ) {
 // Define specific configuration for English Wikipedia
 window.LanguageConverter.config = {
 	msg: {
-		error_missing_dict	 : 'The following dictionary was not found:\n',
-		error_missing_dict_name : 'It is necessary to define the page name of the dictionary for ',
-		error_word_processing	 : 'Error has occurred while processing the following word:\n',
-		error_typo_processing	 : 'Error has occurred while processing the following typographic change:\n',
-		help_page_link		 : 'Open help page',
-		show_changes_link	 : 'Show changes',
-		hide_changes_link	 : 'Hide changes',
-		menu_title		 : 'Variants'
+		error_missing_dict: 'The following dictionary was not found:\n',
+		error_missing_dict_name: 'It is necessary to define the page name of the dictionary for ',
+		error_word_processing: 'Error has occurred while processing the following word:\n',
+		error_typo_processing: 'Error has occurred while processing the following typographic change:\n',
+		help_page_link: 'Open help page',
+		show_changes_link: 'Show changes',
+		hide_changes_link: 'Hide changes',
+		menu_title: 'Variants'
 	},
 
 	/**
 	* What characters are considered part of words
 	* Do you need every character in this list? Any other?
 	*/
-	word_chars	 : 'a-zA-Z\'-',
+	word_chars: 'a-zA-Z\'-',
 
 	/**
 	* Names of each language and/or variant to which conversion is enabled
 	* in the current wiki
 	*/
-	variants_list : {
-		'en':'Original spellings', // Default language == mw.config.get('wgContentLanguage')
-		'en-gb':'British spellings',
-		'en-us':'American spellings'
+	variants_list: {
+		'en': 'Original spellings', // Default language == mw.config.get('wgContentLanguage')
+		'en-gb': 'British spellings',
+		'en-us': 'American spellings'
 	},
 
 	/**
 	* Typographic conversion (applied before dictionary conversion)
 	* Accepts regular expressions
 	*/
-	typo_changes : {
+	typo_changes: {
 		/*
 		// Each variant can have its own list of typographic conversions to be performed
 		'en-gb': [
@@ -53,7 +53,7 @@ window.LanguageConverter.config = {
 	* the presence of an element whose id attribute is
 	* the value of local_dic_id
 	*/
-	ns_list : {
+	ns_list: {
 		'0': true, // Main
 		'2': true, // User
 		'4': true, // Wikipedia
@@ -67,50 +67,50 @@ window.LanguageConverter.config = {
 	* 1) To enable conversion to some variant; and/or
 	* 2) To define local dictionaries (valid through a specific article)
 	*/
-	local_dic_id : {
-		'en':'lang-conv',
-		'en-gb':'local-dic-GB',
-		'en-us':'local-dic-US'
+	local_dic_id: {
+		'en': 'lang-conv',
+		'en-gb': 'local-dic-GB',
+		'en-us': 'local-dic-US'
 	},
 	/**
 	* Class used in template or wikitext to disable conversion
 	* of the content inside of an HTML element
 	*/
-	no_conversion_class : 'no-conversion',
+	no_conversion_class: 'no-conversion',
 
 	/**
 	* Pages of current wiki where the dictionaries for each variant should be defined
 	*/
-	global_dic_page : {
-		'en-gb':'Wikipedia:WikiProject User scripts/Scripts/Language Converter/Dictionary/en-GB',
-		'en-us':'Wikipedia:WikiProject User scripts/Scripts/Language Converter/Dictionary/en-US'
+	global_dic_page: {
+		'en-gb': 'Wikipedia:WikiProject User scripts/Scripts/Language Converter/Dictionary/en-GB',
+		'en-us': 'Wikipedia:WikiProject User scripts/Scripts/Language Converter/Dictionary/en-US'
 	},
 
 	/**
 	* Help page about the conversion system
 	*/
-	help_page	 : 'Wikipedia:WikiProject User scripts/Scripts/Language Converter',
+	help_page: 'Wikipedia:WikiProject User scripts/Scripts/Language Converter',
 
 	/**
 	* Enables the addition of a span around each converted expression
 	* (for dictionary, not typographic conversion)
 	*/
-	show_changes	 : false,
+	show_changes: false,
 
 	/**
 	* Enables the exibition of the text of selected item in the menu title
 	*/
-	show_menu_title	 : true,
+	show_menu_title: true,
 
 	/**
 	* Defines how to select pages where the conversion is enabled
 	*/
-	selection_mode	 : 'OR',
+	selection_mode: 'OR',
 
 	/**
 	* How many words in sequence can be converted as a "phrase"
 	*/
-	max_seq		 : 3
+	max_seq: 3
 }; // Config
 
 // Import [[oldwikisource:User:He7d3r/Tools/LanguageConverter.js]] to allow language conversion
